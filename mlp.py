dc8ae7fab20e6c96039fb7fd217ae7044de9de65
reasimei <99545868+reasimei@users.noreply.github.com>
2024-12-29 17:26:28 +0800

    Update mlp.py

--- mlp.py
+++ mlp.py
@@ -9,10 +9,11 @@ class NetMLP(torch.nn.Module):
 
     def __init__(self):
         super().__init__()
-        self.fc1 = torch.nn.Linear(28*28, 64)
+        #4个全连接层
+        self.fc1 = torch.nn.Linear(28*28, 64) #输入28x28
         self.fc2 = torch.nn.Linear(64, 64)
         self.fc3 = torch.nn.Linear(64, 64)
-        self.fc4 = torch.nn.Linear(64, 10)
+        self.fc4 = torch.nn.Linear(64, 10) #输出0-9共10种可能
     
     def forward(self, x):
         x = torch.nn.functional.relu(self.fc1(x))
@@ -21,13 +22,13 @@ class NetMLP(torch.nn.Module):
         x = torch.nn.functional.log_softmax(self.fc4(x), dim=1)
         return x
 
-
+#下载数据集MNIST
 def get_data_loader(is_train):
     to_tensor = transforms.Compose([transforms.ToTensor()])
     data_set = MNIST("", is_train, transform=to_tensor, download=True)
     return DataLoader(data_set, batch_size=15, shuffle=True)
 
-
+#评估实验结果-正确率
 def evaluate(test_data, net):
     n_correct = 0
     n_total = 0
@@ -47,9 +48,9 @@ def main():
     test_data = get_data_loader(is_train=False)
     net = NetMLP()
 
-
     print("initial accuracy:", evaluate(test_data, net))
     optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
+    #训练两次提高数据利用率
     for epoch in range(2):
         for (x, y) in train_data:
             net.zero_grad()
@@ -58,7 +59,7 @@ def main():
             loss.backward()
             optimizer.step()
         print("epoch", epoch, "accuracy:", evaluate(test_data, net))
-
+    #可视化
     for (n, (x, _)) in enumerate(test_data):
         if n > 3:
             break
